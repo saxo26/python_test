faea9615811502d0863ca2270aafd8abae3ac259
Shawn <shawntseng@systex.com>
2024-12-24 16:37:31 +0800

    test python

---
+++ test001.py
@@ -0,0 +1,3 @@
+print('test python')
+print(2+3)
+print(5*6)
---
+++ test002.py
@@ -0,0 +1,8 @@
+print('calculate the average of two nums')
+
+num1 = float(input("Please enter the first number :  "))
+num2 = float(input("Please enter the second number :  "))
+avg = (num1 + num2) / 2
+
+print('The nums are ',num1,' and ',num2 )
+print('The average is :  ', avg)
